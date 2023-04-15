584b52b0ce2f6a24856413f2ecb02c21cf101ae2
Mark Nair <marknair@me.com>
2023-04-15 16:51:39 -0500

    Initial Commit

---
+++ State/StateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateApp.swift
+//  State
+//
+//  Created by Mark Nair on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct StateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
